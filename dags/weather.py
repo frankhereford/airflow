45b6b098481696ebc79232654f730996d9f9c335
Frank Hereford <frankhereford@users.noreply.github.com>
2023-04-12 18:47:54 -0500

    test dag update

--- dags/weather.py
+++ dags/weather.py
@@ -88,7 +88,7 @@ def etl_weather():
                     justify-content: center;
                     align-items: center;
                     height: 100vh;
-                    background-color: lightblue;
+                    background-color: lightgreen;
                 }}
                 .weather {{
                     font-weight: bold;
