4af6072a3ae97278b282cdd12aad9511cbb1bf70
Frank Hereford <frank@frankhereford.com>
2023-04-12 06:00:59 +0000

    and you write a webhook

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -1,13 +1,25 @@
+import json
 from flask import Flask, request
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
     payload = request.get_json()
-    print(payload)
+    with open("/app/filename.txt", "w") as file:
+        file.write("Webhook post\n")
+        file.write(json.dumps(payload))
+        file.write("\n")
+        file.flush()
     # Process the payload data here
     return 'Received webhook payload'
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
+# here
